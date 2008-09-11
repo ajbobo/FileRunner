5e42c730bd5622b8f59a1cfcdf8d9b98d20c205a
ABobo <none@none>
2008-09-11 15:20:54 +0000

    Added VS2008 project to repository Operations that work:    Display file/directory names    Set MP3 track numbers    Delete Flexsim backup files    Delete CVS backup files/folders Operations that don't quite work:    Delete SVN directories

---
+++ FileRunner/MainGUI.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FileRunner
+{
+	partial class MainGUI
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGUI));
+			this.txtResults = new System.Windows.Forms.TextBox();
+			this.btnDoIt = new System.Windows.Forms.Button();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblOperation = new System.Windows.Forms.Label();
+			this.cboOperation = new System.Windows.Forms.ComboBox();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.btnRecurse = new System.Windows.Forms.CheckBox();
+			this.btnShowSystem = new System.Windows.Forms.CheckBox();
+			this.btnShowHidden = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// txtResults
+			// 
+			this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+							| System.Windows.Forms.AnchorStyles.Left)
+							| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtResults.Location = new System.Drawing.Point(12, 88);
+			this.txtResults.Multiline = true;
+			this.txtResults.Name = "txtResults";
+			this.txtResults.ReadOnly = true;
+			this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtResults.Size = new System.Drawing.Size(634, 329);
+			this.txtResults.TabIndex = 9;
+			// 
+			// btnDoIt
+			// 
+			this.btnDoIt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnDoIt.Location = new System.Drawing.Point(494, 59);
+			this.btnDoIt.Name = "btnDoIt";
+			this.btnDoIt.Size = new System.Drawing.Size(75, 23);
+			this.btnDoIt.TabIndex = 8;
+			this.btnDoIt.Text = "Do It!";
+			this.btnDoIt.UseVisualStyleBackColor = true;
+			this.btnDoIt.Click += new System.EventHandler(this.btnDoIt_Click);
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowse.Location = new System.Drawing.Point(615, 10);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(31, 23);
+			this.btnBrowse.TabIndex = 7;
+			this.btnBrowse.Text = "...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txtPath
+			// 
+			this.txtPath.AllowDrop = true;
+			this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+							| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtPath.Location = new System.Drawing.Point(12, 12);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(597, 20);
+			this.txtPath.TabIndex = 6;
+			this.txtPath.Text = "D:\\Music";
+			this.txtPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtPath_DragDrop);
+			this.txtPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPath_DragEnter);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(-111, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(29, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Path";
+			// 
+			// lblOperation
+			// 
+			this.lblOperation.AutoSize = true;
+			this.lblOperation.Location = new System.Drawing.Point(9, 64);
+			this.lblOperation.Name = "lblOperation";
+			this.lblOperation.Size = new System.Drawing.Size(56, 13);
+			this.lblOperation.TabIndex = 10;
+			this.lblOperation.Text = "Operation:";
+			// 
+			// cboOperation
+			// 
+			this.cboOperation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+							| System.Windows.Forms.AnchorStyles.Right)));
+			this.cboOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboOperation.FormattingEnabled = true;
+			this.cboOperation.Location = new System.Drawing.Point(71, 61);
+			this.cboOperation.Name = "cboOperation";
+			this.cboOperation.Size = new System.Drawing.Size(417, 21);
+			this.cboOperation.TabIndex = 11;
+			// 
+			// btnClear
+			// 
+			this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnClear.Location = new System.Drawing.Point(572, 59);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 12;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// btnRecurse
+			// 
+			this.btnRecurse.AutoSize = true;
+			this.btnRecurse.Location = new System.Drawing.Point(12, 38);
+			this.btnRecurse.Name = "btnRecurse";
+			this.btnRecurse.Size = new System.Drawing.Size(122, 17);
+			this.btnRecurse.TabIndex = 13;
+			this.btnRecurse.Text = "Scan sub-directories";
+			this.btnRecurse.UseVisualStyleBackColor = true;
+			// 
+			// btnShowSystem
+			// 
+			this.btnShowSystem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnShowSystem.AutoSize = true;
+			this.btnShowSystem.Location = new System.Drawing.Point(422, 38);
+			this.btnShowSystem.Name = "btnShowSystem";
+			this.btnShowSystem.Size = new System.Drawing.Size(109, 17);
+			this.btnShowSystem.TabIndex = 14;
+			this.btnShowSystem.Text = "Show system files";
+			this.btnShowSystem.UseVisualStyleBackColor = true;
+			// 
+			// btnShowHidden
+			// 
+			this.btnShowHidden.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnShowHidden.AutoSize = true;
+			this.btnShowHidden.Location = new System.Drawing.Point(537, 38);
+			this.btnShowHidden.Name = "btnShowHidden";
+			this.btnShowHidden.Size = new System.Drawing.Size(109, 17);
+			this.btnShowHidden.TabIndex = 15;
+			this.btnShowHidden.Text = "Show hidden files";
+			this.btnShowHidden.UseVisualStyleBackColor = true;
+			// 
+			// MainGUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(658, 427);
+			this.Controls.Add(this.btnShowHidden);
+			this.Controls.Add(this.btnShowSystem);
+			this.Controls.Add(this.btnRecurse);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.cboOperation);
+			this.Controls.Add(this.lblOperation);
+			this.Controls.Add(this.txtResults);
+			this.Controls.Add(this.btnDoIt);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainGUI";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "File Runner";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtResults;
+		private System.Windows.Forms.Button btnDoIt;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblOperation;
+		private System.Windows.Forms.ComboBox cboOperation;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.CheckBox btnRecurse;
+		private System.Windows.Forms.CheckBox btnShowSystem;
+		private System.Windows.Forms.CheckBox btnShowHidden;
+	}
+}
+
---
+++ FileRunner/MainGUI.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using folderSelect;
+using HundredMilesSoftware.UltraID3Lib;
+
+namespace FileRunner
+{
+	public delegate void FileFunction(FileInfo curfile, int index);
+	public delegate void DirectoryFunction(DirectoryInfo curdir);
+	
+	public partial class MainGUI : Form
+	{
+		public MainGUI()
+		{
+			InitializeComponent();
+
+			// Put the possible operations in the combo box
+			cboOperation.Items.Add(new Operation("Show file name", ShowDirectoryName, ShowFileName));
+			cboOperation.Items.Add(new Operation("Set mp3 track number", ShowDirectoryName, SetMp3Track));
+			cboOperation.Items.Add(new Operation("Delete Flexsim backup files (.*!)", ShowDirectoryName, DeleteFlexsimBackupFile));
+			cboOperation.Items.Add(new Operation("Delete SVN directories (.svn)", DeleteSVNDirectory, ShowFileName));
+			cboOperation.Items.Add(new Operation("Delete CVS backup files (.# files)", ShowDirectoryName, DeleteCVSBackUpFile));
+			cboOperation.Items.Add(new Operation("Delete CVS folders", DeleteCVSDirectory, ShowFileName));
+			cboOperation.Items.Add(new Operation("Delete all CVS remnants (files & folders)", DeleteCVSDirectory, DeleteCVSBackUpFile));
+
+			// Pick a default operation
+			cboOperation.SelectedIndex = 0;
+		}
+
+		private void txtPath_DragDrop(object sender, DragEventArgs e)
+		{
+			// Copies the name of the file that's being dragged into the control
+			string[] strdata = (string[])e.Data.GetData(DataFormats.FileDrop);
+			txtPath.Text = strdata[0];
+		}
+
+		private void txtPath_DragEnter(object sender, DragEventArgs e)
+		{
+			// Checks to see if a file is being dragged over the control - files will be accepted, other formats won't be
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				if ((e.AllowedEffect & DragDropEffects.Copy) != 0)
+				{
+					e.Effect = DragDropEffects.Copy;
+				}
+			}
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			FolderSelect dlg = new FolderSelect();
+
+			dlg.ShowDialog();
+
+			txtPath.Text = dlg.fullPath;
+		}
+
+		private void btnDoIt_Click(object sender, EventArgs e)
+		{
+			txtResults.Clear();
+
+			DoDirectory(txtPath.Text);
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			txtResults.Clear();
+		}
+
+		private void DoDirectory(string directory)
+		{
+			// Find the Operation to peform on this directory/file
+			Operation op = (Operation)cboOperation.SelectedItem;
+			if (op == null)
+			{
+				txtResults.Text = "No Operation Selected";
+				return;
+			}
+
+			// Find the current directory - perform the selected operation on it
+			DirectoryInfo topdir = new DirectoryInfo(directory);
+			if (!topdir.Exists)
+			{
+				MessageBox.Show(directory, "Directory doesn't exist");
+				return;
+			}
+			op.OperatateOnDirectory(topdir);
+			txtResults.Text += "\r\n";
+			ScrollToEnd();
+
+			// Look through the files - perform the selected operation on them
+			try
+			{
+				FileInfo[] files = topdir.GetFiles();
+				int index = 0;
+				foreach (FileInfo curfile in files)
+				{
+					bool issystem = HasAttribute(curfile.Attributes, FileAttributes.System);
+					bool ishidden = HasAttribute(curfile.Attributes, FileAttributes.Hidden);
+					bool showit = false;
+					if (!issystem && !ishidden) // Show normal files
+						showit = true;
+					else if (issystem && btnShowSystem.Checked) // If system files are supposed to be visible, show them
+						showit = true;
+					else if (ishidden && !issystem && btnShowHidden.Checked) // The "Show Hidden" checkbox won't show system files
+						showit = true;
+
+					if (showit)
+					{
+						op.OperatateOnFile(curfile, index);
+						txtResults.Text += "\r\n";
+						ScrollToEnd();
+						index++;
+					}
+				}
+
+				// Recurse through the subdirectories
+				if (btnRecurse.Checked)
+				{
+					DirectoryInfo[] dirs = topdir.GetDirectories();
+					foreach (DirectoryInfo curdir in dirs)
+					{
+						DoDirectory(curdir.FullName);
+					}
+				}
+			}
+			catch (DirectoryNotFoundException ex)
+			{
+				// This happens if the directory was deleted
+				string dummy = ex.Message;
+			}
+		}
+
+		private void ScrollToEnd()
+		{
+			txtResults.SelectionStart = txtResults.Text.Length;
+			txtResults.ScrollToCaret();
+		}
+
+		bool HasAttribute(FileAttributes value, FileAttributes check)
+		{
+			if ((value & check) == check)
+				return true;
+
+			return false;
+		}
+
+		/****************************************************
+		 * Functions that can be put in the Operations list *
+		 ****************************************************/
+		private void ShowDirectoryName(DirectoryInfo curdir)
+		{
+			txtResults.Text += "Directory: " + curdir.FullName;
+		}
+
+		private void ShowFileName(FileInfo curfile, int index)
+		{
+			txtResults.Text += "  " + index + " - " + curfile.Name;
+			if (HasAttribute(curfile.Attributes,FileAttributes.System))
+				txtResults.Text += " - System";
+			if (HasAttribute(curfile.Attributes, FileAttributes.Hidden))
+				txtResults.Text += " - Hidden";
+		}
+
+		private void SetMp3Track(FileInfo curfile, int index)
+		{
+			ShowFileName(curfile, index);
+
+			// Set the new track number for the file
+			UltraID3 ultraid3 = new UltraID3();
+			int trackcnt = index + 1;
+			try
+			{
+				ultraid3.Read(curfile.FullName);
+				int oldnum = (int)ultraid3.TrackNum; // Get the old track number
+				txtResults.Text += "\tOldTrack: " + oldnum + "  NewTrack: " + trackcnt;
+				ultraid3.TrackNum = (short)trackcnt; // Set the new track number
+				ultraid3.Write(); // Write the changes to the file
+			}
+			catch (Exception ex)
+			{
+				txtResults.Text += "    ************* EXCEPTION *************\r\n    ";
+				txtResults.Text += ex.Message;
+			}
+		}
+
+		private void DeleteFlexsimBackupFile(FileInfo curfile, int index)
+		{
+			ShowFileName(curfile, index);
+
+			string filename = curfile.Name;
+			if (filename[filename.Length - 1] == '!') // It's a backup file
+			{
+				txtResults.Text += " -> Backup file DELETED #####";
+				curfile.Delete();
+			}
+		}
+
+		private void DeleteCVSBackUpFile(FileInfo curfile, int index)
+		{
+			ShowFileName(curfile, index);
+
+			string filename = curfile.Name;
+			if (filename.StartsWith(".#")) // It's a CVS backup file
+			{
+				txtResults.Text += " -> Backup file DELETED #####";
+				curfile.Delete();
+			}
+		}
+
+		private void DeleteCVSDirectory(DirectoryInfo curdir)
+		{
+			ShowDirectoryName(curdir);
+
+			string dirname = curdir.Name;
+			if (dirname == "CVS")
+			{
+				txtResults.Text += " -> Directory DELETED #####";
+				curdir.Delete(true); // Deletes sub-directories and files
+			}
+		}
+
+		private void DeleteSVNDirectory(DirectoryInfo curdir)
+		{
+			ShowDirectoryName(curdir);
+
+			string dirname = curdir.Name;
+			try
+			{
+				if (dirname == ".svn")
+				{
+					txtResults.Text += " -> Directory DELETED #####";
+					curdir.Delete(true); // Deletes sub-directories and files
+				}
+			}
+			catch (Exception ex)
+			{
+				txtResults.Text += "    ************* EXCEPTION *************\r\n    ";
+				txtResults.Text += ex.Message;
+			}
+		}
+	}
+
+	/*********************************************************************
+	 * Represents an operation that can be performed on a file/directory *
+	 *********************************************************************/
+	public class Operation
+	{
+		private string name;
+		private FileFunction filefunc;
+		private DirectoryFunction dirfunc;
+
+		public Operation(string name, DirectoryFunction dirfunc, FileFunction filefunc)
+		{
+			this.name = name;
+			this.filefunc = filefunc;
+			this.dirfunc = dirfunc;
+		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+
+		public void OperatateOnFile(FileInfo curfile, int index)
+		{
+			if (filefunc != null)
+				filefunc(curfile, index);
+		}
+
+		public void OperatateOnDirectory(DirectoryInfo curdir)
+		{
+			if (dirfunc != null)
+				dirfunc(curdir);
+		}
+	}
+}
